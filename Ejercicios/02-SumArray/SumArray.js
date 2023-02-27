6b69c33c130c1bb7b91754208268839805dfc7e0
fiorella frini <fiorella_frini@hotmail.com>
2023-02-27 11:05:32 -0300

    02

--- Ejercicios/02-SumArray/SumArray.js
+++ Ejercicios/02-SumArray/SumArray.js
@@ -1,6 +1,31 @@
+//-------------------SOLUCIÓN CON FUERZA BRUTA---------------
 function SumArray (arr, n) {
   // Your code here:
 
+  for(let i = 0; i < arr.length; i++){
+  }
+  
+  for(let j = i + 1; j < arr2.length; j++){
+    if(arr[i] + arr[j] === n) return true;
+  }
+  return false;
 }
+//COMPLEJIDAD >>> O(n^2)
 
-module.exports = SumArray
+//-----------------REFACTOR--------------------------
+function SumArray(arr, n){
+  let start = 0;
+  let end = arr.length - 1;
+
+  while(start !== end){
+    const sum = arr[start] + arr[end];
+    if(sum === n) return true;
+    if(sum > n) end--;
+    else start++;
+  }
+
+  return false;
+}
+// COMPLEJIDAD >>> O(n)
+
+module.exports = SumArray;
